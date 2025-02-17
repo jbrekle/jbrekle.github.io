d743f2e9235ec4dc4065096b4d75f1a86799ff53
Jonas Brekle <jonas.brekle@gmail.com>
2025-02-17 22:07:26 +0100

    fix paths

--- game-showcase/vite.config.js
+++ game-showcase/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // Use a relative base so that built asset URLs are relative
   build: {
     // Optional, default is "dist", but let's be explicit
     outDir: 'dist',
