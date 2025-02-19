713ddae3d906a71ef1c69243bff2f45571b46653
Jonas Brekle <jonas.brekle@gmail.com>
2025-02-20 00:43:32 +0100

    fix dynamically loaded tenant config

--- wizard-showcase/vite.config.js
+++ wizard-showcase/vite.config.js
@@ -1,7 +1,23 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  base: '', 
-  plugins: [react()]
+  base: '',
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        // directly to the build "dist" folder with no modifications.
+        { src: 'src/configs/tenant-*-config.ts', dest: 'dist/assets/configs' }
+      ],
+      // Use hook: 'writeBundle' to ensure the copy happens after the build is done
+      hook: 'writeBundle'
+    })
+  ], 
+  resolve: {
+    alias     : {
+        'assets/configs/tenant-steuerberater-config': "assets/configs/tenant-steuerberater-config.ts"
+      }
+    }
 })
