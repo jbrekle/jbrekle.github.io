5bb3e1ef3ee7137b749991317c834cbbf2b96456
Jonas Brekle <jonas.brekle@gmail.com>
2025-02-20 17:13:13 +0100

    validation fix

--- wizard-showcase/src/helpers/wizardHelpers.ts
+++ wizard-showcase/src/helpers/wizardHelpers.ts
@@ -1,12 +1,5 @@
 ﻿import { Config } from "../configTypes";
 
-/**
- * Checks if an answer satisfies a given abort condition.
- * @param answer The provided answer.
- * @param comparer The comparer string.
- * @param conditionValue The value to compare against.
- * @returns true if condition is satisfied.
- */
 export function satisfiesCondition(answer: any, comparer: string, conditionValue: any): boolean {
   switch (comparer) {
     case 'equal':
@@ -34,14 +27,11 @@ export function satisfiesCondition(answer: any, comparer: string, conditionValue
   }
 }
 
-/**
- * Checks the abort conditions on the current page.
- * @param pages Array of pages from the config.
- * @param currentPage Current page index.
- * @param answers Object containing answers.
- * @returns The abort explanation string if a condition is met; otherwise, null.
- */
-export function checkAbortConditions(pages: Config["pages"], currentPage: number, answers: { [key: string]: any }): string | null {
+export function checkAbortConditions(
+  pages: Config["pages"],
+  currentPage: number,
+  answers: { [key: string]: any }
+): string | null {
   if (currentPage < pages.length) {
     for (const section of pages[currentPage].sections) {
       if (section.questions) {
@@ -60,28 +50,20 @@ export function checkAbortConditions(pages: Config["pages"], currentPage: number
   return null;
 }
 
-/**
- * Runs validation on the current page.
- * @param pages Array of pages from the config.
- * @param currentPage Current page index.
- * @param touched Object of touched fields.
- * @param answers Object of answers.
- * @param validate A function to validate a question.
- * @returns true if any field has errors, otherwise false.
- */
 export function runPageValidation(
   pages: Config["pages"],
   currentPage: number,
   touched: { [key: string]: boolean },
   answers: { [key: string]: any },
-  validate: (question: any, value: any) => string
+  validate: (question: any, value: any) => string,
+  force: boolean = false
 ): boolean {
   let pageHasErrors = false;
   if (currentPage < pages.length) {
     pages[currentPage].sections.forEach(section => {
       if (section.questions) {
         section.questions.forEach(question => {
-          if (touched[question.id]) {
+          if (force || touched[question.id]) {
             const error = validate(question, answers[question.id]);
             if (error) {
               pageHasErrors = true;
